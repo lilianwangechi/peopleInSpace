0b1c7fd3907b7ee65250f1124a8eccbb5b8d7b44
lilian <lilian.wangechi@student.moringaschool.com>
2022-09-29 18:49:11 +0300

    display a list of people in space fetch from the api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect,useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [peopleInSpace,setPeopleInSpace]= useState([]);
+  const [isLoaded,setIsLoaded]=useState(false);
+  useEffect(()=>{
+    fetch("http://api.open-notify.org/astros.json")
+    .then((response)=>response.json())
+    .then((data)=>{
+      setPeopleInSpace(data.people);
+      setIsLoaded(true);
+    });
+  },[]);
+   //use an empty dependencies array so we only run the fetch request once
+  if(!isLoaded)return<h3>Loading...</h3>
+   return (
+  <div>
+    <ol>
+    {peopleInSpace.map((person)=>
+    <li>{person.name}</li>)}</ol>
     </div>
   );
 }
